8a20160576bbcb9d33fa4472a24c860be9602d0a
Asad Zia <asaad_zia@hotmail.com>
2015-04-23 02:13:27 +0200

    Create Parenthesis.c

---
+++ Strings/Parenthesis.c
@@ -0,0 +1,158 @@
+/*
+* A program to check if the parenthesis are complete in an expression in C
+*
+* Created by Asad Zia
+*/
+
+#define MAX 20
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* the structure defiend for the stack */
+struct stack
+{
+	int count;
+	char *container;
+};
+
+/* 
+* Initializing the stack
+*/
+void initialize (struct stack* x)
+{
+	x->container = (char*)malloc(sizeof(char) * MAX);
+	if (x->container == NULL)
+	{
+		printf("Error in memory allocation!\n");
+		exit(1);
+	}
+	x->count = 0;
+}
+
+/*
+* The push function
+*/
+void push (struct stack* x, char y)
+{
+	int flag = 0;
+	if (x == NULL)
+	{
+		printf("Stack does not exist!\n");
+		exit(1);
+	}
+
+	if (x->count == MAX)
+	{
+		printf("Stack Overflow!\n");
+		flag = 1;
+	}
+
+	if (flag == 0)
+	{
+		x->count++;
+		x->container[x->count] = y;
+	}
+}
+
+/*
+* The pop fucntion
+*/
+char pop (struct stack* x)
+{
+	int flag = 0;
+	if (x == NULL)
+	{
+		printf("Stack does not exist!\n");
+		exit(1);
+	}
+
+	if (x->count == 0)
+	{
+		printf("Stack Underflow\n");
+		flag = 1;
+	}
+
+	if (flag == 0)
+	{
+		char result = x->container[x->count];
+		x->count--;
+		return result;
+	}
+}
+
+/*
+* The function to check the balance of the parenthesis
+*/
+int parenthesisBalance (char x, char y)
+{
+	if (x == '{' && y == '}') 
+	{
+		return 0;
+	}
+	else if (x == '(' && y == ')')
+	{
+		return 0;
+	}
+	else if (x == '[' && y == ']'){
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+/*
+* The function for deciding if the parenthesis are indeed balanced in a string
+*/
+void checkParenthesis (struct stack* x, char* str)
+{
+	int i, flag = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == '{' || str[i] == '[' || str[i] == '(')
+		{
+			push(x, str[i]); 
+
+		}
+
+		if (str[i] == '}' || str[i] == ']' || str[i] == ')')
+		{
+			char result = pop(x);
+			int answer = parenthesisBalance(result, str[i]);
+			if (answer == 1)
+			{
+				printf("Parenthesis are not balanced!\n");
+				flag = 1;
+				break;
+			} 
+
+		}
+	}
+	if (flag == 0) 
+	{
+		printf("Parenthesis are balanced!\n");
+	}
+}
+
+/*
+* Deallocate the stack memory
+*/
+void freeStack (struct stack *x)
+{
+	free(x->container);
+}
+
+/*
+* The main function
+*/
+int main ()
+{
+	struct stack myStack;
+	char* str = "[{()}]()[{}]";
+	initialize(&myStack);
+	checkParenthesis(&myStack, str);
+	freeStack(&myStack);
+	return 0;
+}
