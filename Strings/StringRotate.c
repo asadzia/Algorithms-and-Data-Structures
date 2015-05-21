f8989a2da8d8e344520fe866648133fc75ff28b4
Asad Zia <asaad_zia@hotmail.com>
2015-05-21 12:18:13 +0200

    Create StringRotate.c
    A program to rotate a string by a value k in C.

---
+++ Strings/StringRotate.c
@@ -0,0 +1,42 @@
+/*
+* A function to rotate a string in C.
+*
+*
+*
+* Created by: Asad zia
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void reverse (char* str, int start, int end)
+{
+	char temp;
+
+	while (start < end)
+	{
+		temp = str[start];
+		str[start] = str[end];
+		str[end] = temp;
+		start++;
+		end--;
+	}
+}
+
+void rotate (char* str, int k)
+{
+	int j = strlen(str);
+	reverse(str, 0, j - 1);
+	reverse(str, 0, k - 1);
+	reverse(str, k, j - 1);
+}
+
+int main ()
+{
+	char str[20] = "DeadBones";
+	printf("Original String: %s\n", str);
+	rotate(str, 2);
+	printf("Rotated String: %s\n", str);
+	return 0;
+}
