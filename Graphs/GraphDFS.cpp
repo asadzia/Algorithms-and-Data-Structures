f48f2fb02cacd94d2140655115401feeed6a7e58
Asad Zia <asaad_zia@hotmail.com>
2015-05-31 02:48:21 +0200

    Update GraphDFS.cpp

--- Graphs/GraphDFS.cpp
+++ Graphs/GraphDFS.cpp
@@ -85,8 +85,8 @@ void Graph::DFS (int v)
 */
 int main ()
 {
-	Graph myGraph(4);
-	myGraph.addEdge(0, 1);
+    Graph myGraph(4);
+    myGraph.addEdge(0, 1);
     myGraph.addEdge(0, 2);
     myGraph.addEdge(1, 2);
     myGraph.addEdge(2, 0);
