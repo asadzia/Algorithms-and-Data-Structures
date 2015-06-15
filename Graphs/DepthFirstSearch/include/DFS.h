9e22534d4fea7b9540bed5a3aec5c5593d6ba874
Asad Zia <asaad_zia@hotmail.com>
2015-06-15 23:39:56 +0200

    Create DFS.h

---
+++ Graphs/DepthFirstSearch/include/DFS.h
@@ -0,0 +1,31 @@
+/*
+*
+* A C++ implmentation of Depth first search on a directed graph.
+*
+* Created by: Asad zia.
+*/
+
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+class Graph
+{
+	public:
+	/* number of vertices in the graph */
+	int Vertices;
+	/* the adjacency list for each vertex */
+	list<int> *adj;
+	/* a helper function for implementing DFS */
+	void DFSHelper(int x, bool visitedNodes[]);
+
+  /* The constructor */
+	Graph(int v);
+	/* tje destructor */
+	~Graph();
+	/* the function for adding an edge */
+	void addEdge(int src, int dest);
+	/* the function for implementing DFS */
+	void DFS(int src);
+};
