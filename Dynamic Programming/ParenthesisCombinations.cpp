db894fb1e3324c3d07d0a54611e16025125a6a1a
Asad Zia <asaad_zia@hotmail.com>
2017-05-01 21:16:44 +0100

    Update ParenthesisCombinations.cpp

--- Dynamic Programming/ParenthesisCombinations.cpp
+++ Dynamic Programming/ParenthesisCombinations.cpp
@@ -1,1 +1,40 @@
+/*
+Description: A program which prints all the combinations of parenthesis for n-pairs.
+			 Implemented in C++.
+Version:	 1.0
+Author:		 Asad Zia
+*/
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void printParenthesis (string str, int x, int y, int n)
+{
+	if (x == n && y == n)
+	{
+		cout << str << endl;
+		return;
+	}
+	else
+	{
+		if (x < n)
+		{
+			printParenthesis(str + "(", x + 1, y, n);
+		}
+
+		if (y < x)
+		{
+			printParenthesis(str + ")", x, y + 1, n);
+		}
+	}
+}
+
+int main ()
+{
+	cout << "Begin Execution" << endl;
+	printParenthesis("", 0, 0, 3);
+	cout << "End Execution" << endl;
+	return 0;
+}
