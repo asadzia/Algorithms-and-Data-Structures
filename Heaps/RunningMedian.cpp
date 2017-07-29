b86627b59847c4874d98a81b0f4af50746b7fe79
Asad Zia <asaad_zia@hotmail.com>
2017-07-29 15:07:44 +0100

    Create RunningMedian.cpp

---
+++ Heaps/RunningMedian.cpp
@@ -0,0 +1,114 @@
+/* The running medium problem*/
+
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <functional>
+
+#define DIVISION_POINT 100
+#define SIZE 9
+
+using namespace std;
+
+
+// Mid point is 100
+class RunningMedianAlgorithm
+{
+	private:
+		priority_queue<int, vector<int>, greater<int>> Qone;	//min heap for higher portion of numbers
+		priority_queue<int> Qtwo;								//max heap for lower portion of numbers
+
+	public:
+		static const int length = SIZE;
+		int arr[length];
+		int size = 0;
+		
+		void addNumber (int val)
+		{
+			arr[size++] = val;
+		}
+
+		void initHeaps (int num)
+		{
+				if (Qtwo.size() == 0 || num < Qtwo.top())		
+				{
+					Qtwo.push(num);	// we need max element here
+				}
+				else
+				{
+					Qone.push(num);	// and min element here
+				}
+		}
+
+		int findMedian ()
+		{	
+			int median;
+
+			if ((Qone.size() == Qtwo.size()))
+			{
+				median = (Qone.top() + Qtwo.top()) / 2;
+ 			}
+ 			else
+ 			{
+ 				if (Qone.size() > Qtwo.size())
+ 				{
+ 					median = Qone.top();
+ 				}
+ 				else
+ 				{
+ 					median = Qtwo.top();
+ 				}
+ 			}
+ 			return median;
+		}
+
+		void rebalance()
+		{
+		    while (Qtwo.size() > Qone.size() + 1)
+		    {
+    			if (Qone.size() > Qtwo.size() + 1)
+    			{	
+    				int x = Qone.top();
+    				Qone.pop();
+    				Qtwo.push(x);
+    			}
+    			else if (Qtwo.size() > Qone.size() + 1)
+    			{
+    				int x = Qtwo.top();
+    				Qtwo.pop();
+    				Qone.push(x);
+    			}
+		    }
+			return;
+		}
+
+		void runningMedian ()
+		{
+			for (int i = 0; i < length; i++)
+			{
+				initHeaps(arr[i]);
+				rebalance();
+				int result = findMedian();
+				cout << result << " " << Qone.size() << " " << Qtwo.size() << endl;
+			}
+		}
+};
+
+int main (int argc, char ** args)
+{
+	RunningMedianAlgorithm myObj;
+	myObj.addNumber(45);
+	myObj.addNumber(76);
+	myObj.addNumber(98);
+	myObj.addNumber(12);
+	myObj.addNumber(5);
+	myObj.addNumber(6);
+	myObj.addNumber(8);
+	myObj.addNumber(1);
+	myObj.addNumber(10);
+
+	myObj.runningMedian();
+
+	return 0;
+}
+
