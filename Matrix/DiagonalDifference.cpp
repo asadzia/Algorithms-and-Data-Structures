25b0568b3837098217ff5adfc370172ada199175
Asad Zia <asaad_zia@hotmail.com>
2018-08-26 20:16:09 +0100

    Create DiagonalDifference.cpp

---
+++ Matrix/DiagonalDifference.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the diagonalDifference function below.
+int diagonalDifference(vector<vector<int>> arr) {
+    int difference = 0;
+    int length = arr.size();
+    
+    for (int i = 0; i < arr.size(); ++i)
+    {
+        difference = difference + (arr[i][i] - arr[i][length - 1 - i]);
+    }
+    
+    return abs(difference);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<vector<int>> arr(n);
+    for (int i = 0; i < n; i++) {
+        arr[i].resize(n);
+
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int result = diagonalDifference(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+
+/*
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+For example, the square matrix  is shown below:
+
+1 2 3
+4 5 6
+9 8 9  
+The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+
+Function description
+
+Complete the  function in the editor below. It must return an integer representing the absolute diagonal difference.
+
+diagonalDifference takes the following parameter:
+
+arr: an array of integers .
+Input Format
+
+The first line contains a single integer, , the number of rows and columns in the matrix . 
+Each of the next  lines describes a row, , and consists of  space-separated integers .
+
+Constraints
+
+Output Format
+
+Print the absolute difference between the sums of the matrix's two diagonals as a single integer.
+
+Sample Input
+
+3
+11 2 4
+4 5 6
+10 8 -12
+Sample Output
+
+15
+Explanation
+
+The primary diagonal is:
+
+11
+   5
+     -12
+Sum across the primary diagonal: 11 + 5 - 12 = 4
+
+The secondary diagonal is:
+
+     4
+   5
+10
+Sum across the secondary diagonal: 4 + 5 + 10 = 19 
+Difference: |4 - 19| = 15
+
+Note: |x| is the absolute value of x
+*/
