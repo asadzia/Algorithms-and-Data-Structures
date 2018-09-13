83d8f9c99905afc11f64835a5c3fd472a2e4e621
Asad Zia <asaad_zia@hotmail.com>
2018-09-13 23:16:03 +0100

    Update StringReverse.c

--- Strings/StringReverse.c
+++ Strings/StringReverse.c
@@ -66,3 +66,35 @@ int main ()
 }
 
 */
+
+/*
+
+ANOTHER ONE
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void reverse (string str, int i)
+{
+	if (i >= str.length())
+	{
+		return;
+	}
+
+	reverse (str, i + 1);
+
+	cout << str[i];
+}
+
+int main ()
+{
+	string str = "hello";
+
+	reverse(str, 0);
+
+	return 0;
+}
+
+*/
