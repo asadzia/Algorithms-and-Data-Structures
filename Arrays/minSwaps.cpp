e6268b61e7b6fc7a1ae97fd45d04dc2b854a5e25
Asad Zia <asaad_zia@hotmail.com>
2018-12-11 22:49:35 +0000

    Create minSwaps.cpp

---
+++ Arrays/minSwaps.cpp
@@ -0,0 +1,174 @@
+/*
+You are given an unordered array consisting of consecutive integers  [1, 2, 3, ..., n] without any duplicates. You are allowed to swap any two elements. You need to find the minimum number of swaps required to sort the array in ascending order.
+
+For example, given the array  we perform the following steps:
+
+i   arr                         swap (indices)
+0   [7, 1, 3, 2, 4, 5, 6]   swap (0,3)
+1   [2, 1, 3, 7, 4, 5, 6]   swap (0,1)
+2   [1, 2, 3, 7, 4, 5, 6]   swap (3,4)
+3   [1, 2, 3, 4, 7, 5, 6]   swap (4,5)
+4   [1, 2, 3, 4, 5, 7, 6]   swap (5,6)
+5   [1, 2, 3, 4, 5, 6, 7]
+It took  swaps to sort the array.
+
+Function Description
+
+Complete the function minimumSwaps in the editor below. It must return an integer representing the minimum number of swaps to sort the array.
+
+minimumSwaps has the following parameter(s):
+
+arr: an unordered array of integers
+Input Format
+
+The first line contains an integer, , the size of . 
+The second line contains  space-separated integers .
+
+Constraints
+
+Output Format
+
+Return the minimum number of swaps to sort the given array.
+
+Sample Input 0
+
+4
+4 3 1 2
+Sample Output 0
+
+3
+Explanation 0
+
+Given array  
+After swapping  we get  
+After swapping  we get  
+After swapping  we get  
+So, we need a minimum of  swaps to sort the array in ascending order.
+
+Sample Input 1
+
+5
+2 3 4 1 5
+Sample Output 1
+
+3
+Explanation 1
+
+Given array  
+After swapping  we get  
+After swapping  we get  
+After swapping  we get  
+So, we need a minimum of  swaps to sort the array in ascending order.
+
+Sample Input 2
+
+7
+1 3 5 2 4 6 7
+Sample Output 2
+
+3
+Explanation 2
+
+Given array  
+After swapping  we get  
+After swapping  we get  
+After swapping  we get  
+So, we need a minimum of  swaps to sort the array in ascending order.
+*/
+
+#include <bits/stdc++.h>
+
+/* Link: https://www.includehelp.com/java-programs/minimum-swaps-required-to-sort-an-array.aspx
+*/
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the minimumSwaps function below.
+int minimumSwaps(vector<int> arr) {
+
+    int count = 0;
+
+    // delcare a vector of bools for defining each position
+    // the index of this vector denotes a number in the original array minus 1
+    vector<bool> vis (arr.size(), false);
+
+    for (int i = 0; i < arr.size(); ++i)
+    {
+        int swaps = 0;
+        int j = i;
+
+        while (!vis[j])
+        {
+            vis[j] = true;
+            j = arr[j] - 1;
+            swaps++;
+        }
+
+        if (swaps > 0)
+        {
+            count += swaps - 1;
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int res = minimumSwaps(arr);
+
+    fout << res << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
