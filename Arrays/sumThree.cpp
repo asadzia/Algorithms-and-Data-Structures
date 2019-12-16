8f8b59cc8eeb1e44f36f6d9f6ab5e2a852420ba3
Asad Zia <asaad_zia@hotmail.com>
2019-12-16 04:29:24 +0000

    Update sumThree.cpp

--- Arrays/sumThree.cpp
+++ Arrays/sumThree.cpp
@@ -13,24 +13,30 @@ The sum that is closest to the target is 2. (-1 + 2 + 1 = 2)
 
 */
 
- int Solution::threeSumClosest(vector<int> &num, int target) {
-            sort(num.begin(), num.end());
-            int bestSum = 1000000000, sum = 0;
-            // Fix the smallest number in the three integers
-            for (int i = 0; i < num.size() - 2; i++) {
-                // Now num[i] is the smallest number in the three integers in the solution
-                int ptr1 = i + 1, ptr2 = num.size() - 1;
-                while (ptr1 < ptr2) {
-                    sum = num[i] + num[ptr1] + num[ptr2];
-                    if (abs(target - sum) < abs(target - bestSum)) {
-                        bestSum = sum;
-                    }
-                    if (sum > target) {
-                        ptr2--;
-                    } else {
-                        ptr1++;
-                    }
-                }
+int Solution::threeSumClosest(vector<int> &A, int B) {
+    sort(A.begin(), A.end());
+    int bestSum = 100000000;
+    
+    for (int i = 0; i < A.size() - 2; ++i) {
+        // fix the minimum value and move it accordingly after subsequent comparisons
+        
+        int ptr1 = i + 1, ptr2 = A.size() - 1;
+        
+        while (ptr1 < ptr2) {
+            
+            int sum = A[ptr1] + A[ptr2] + A[i];
+            
+            if (abs(B - sum) < abs(bestSum - B)) {
+                bestSum = sum;
             }
-            return bestSum;
+            
+            if (sum < B) {
+                ++ptr1;
+            } else {
+                --ptr2;
+            }
         }
+    }
+    
+    return bestSum;
+}
