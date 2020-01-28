ff5ed0c465dc3315b6dc955994bae0f62b5efcff
Asad Zia <asaad_zia@hotmail.com>
2020-01-28 03:19:37 +0000

    Update countSubstringsAnagrams.cpp

--- Strings/countSubstringsAnagrams.cpp
+++ Strings/countSubstringsAnagrams.cpp
@@ -1,5 +1,22 @@
-// C++ program to count total anagram 
-// substring of a string 
+/*
+
+Given a string of lower alphabet characters, count total substring of this string which are anagram to each other.
+
+Examples:
+
+Input  : str = “xyyx”
+Output : 4
+Total substrings of this string which
+are anagram to each other are 4 which 
+can be enumerated as,
+{“x”, “x”}, {"y", "y"}, {“xy”, “yx”}, 
+{“xyy”, “yyx”}
+
+Input  : str = "geeg"
+Output : 4
+
+*/
+
 #include <bits/stdc++.h> 
 using namespace std; 
   
