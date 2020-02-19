1e116ed220487b2d9f5a2d89ec32f6ce3b3682a0
Asad Zia <asaad_zia@hotmail.com>
2020-02-19 23:58:55 +0000

    Create stockBuyAndSell.cpp

---
+++ Arrays/stockBuyAndSell.cpp
@@ -0,0 +1,52 @@
+/*
+Stock Buy Sell to Maximize Profit
+The cost of a stock on each day is given in an array, find the max profit that you can make by buying and selling in those days. For example, if the given array is {100, 180, 260, 310, 40, 535, 695}, the maximum profit can earned by buying on day 0, selling on day 3. Again buy on day 4 and sell on day 6. If the given array of prices is sorted in decreasing order, then profit cannot be earned at all.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// Try to find the local minima and maxima
+void stockBuySell (int arr[], int n) {
+    
+    if (n == 1) {
+        return;
+    }
+    
+    int i = 0;
+    
+    while (i < n - 1) {
+        
+        while (i < n - 1 && arr[i] > arr[i + 1]) {
+            ++i;
+        }
+        
+        int buy = i;
+        
+        while (i < n - 1 && arr[i] < arr[i + 1]) {
+            i++;
+        }
+
+        int sell = i;
+        
+        cout << "(" << buy << " " << sell << ")" << " ";
+    }
+    
+    cout << endl;
+}
+
+
+// Driver code 
+int main() 
+{ 
+    // Stock prices on consecutive days 
+    int price[] = { 100, 180, 260, 310, 40, 535, 695 }; 
+    int n = sizeof(price) / sizeof(price[0]); 
+  
+    // Fucntion call 
+    stockBuySell(price, n); 
+  
+    return 0; 
+} 
